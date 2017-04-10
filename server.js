5c06032351595b97b02954086f8454bef989ed40
Norman Heck <norman.heck@gmail.com>
2017-04-10 15:15:00 +0200

    returned to old version.

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ var express = require('express');
 var app = express();
 
 app.listen(80, function() {
-  console.log('Chatfuel Bot-Server listening on port 80...');
+    console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
 app.get('/*', function(req, res) {
-  res.send({
-    "messages": [
-      { "text": "Hi. your lucky number is:" },
-      { "text": "..." + (Math.random() * 5 + 1).toFixed(0) + "..." }
-    ]
-  });
+    var jsonResponse = [];
+    jsonResponse.push({ "text": "Hi. " + (Math.random() * 5 + 1).toFixed(0) + " is a lucky number..." });
+    res.send(jsonResponse);
 });
