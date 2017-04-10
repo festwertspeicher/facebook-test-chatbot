7e8d3d2df3d2c100c592a902a7181d525a56b6da
Norman Heck <norman.heck@gmail.com>
2017-04-10 14:59:34 +0200

    don't work. make it more simple :(

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ var express = require('express');
 var app = express();
 
 app.listen(80, function() {
-    console.log('Chatfuel Bot-Server listening on port 80...');
+  console.log('Chatfuel Bot-Server listening on port 80...');
 });
 
 app.get('/*', function(req, res) {
-    var jsonResponse = [];
-    jsonResponse.push({
-      "messages": [
-        { "text": "Hi. your lucky number is:" },
-        { "text": "..." + (Math.random() * 5 + 1).toFixed(0) + "..." }
-      ]
-    });
-    res.send(jsonResponse);
+  res.send({
+    "messages": [
+      { "text": "Hi. your lucky number is:" },
+      { "text": "..." + (Math.random() * 5 + 1).toFixed(0) + "..." }
+    ]
+  });
 });
